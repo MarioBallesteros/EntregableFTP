7eb371d9501c02b9f49cdf7455777fab2dbe6a89
MarioBallesteros <16maarioo16@gmail.com>
2024-02-19 22:16:00 +0100

    cambios

--- src/main/java/org/example/LaunchClient.java
+++ src/main/java/org/example/LaunchClient.java
@@ -13,24 +13,21 @@ public class LaunchClient {
             client.connect("192.168.1.45", 2121);
             client.login("anonymous", "");
 
-            String remoteDirPath = "/home/clientessh/pruebaa";
-            if (!client.changeWorkingDirectory(remoteDirPath)) {
-                if (client.makeDirectory(remoteDirPath)) {
-                    System.out.println("Directorio remoto creado con éxito.");
-                    client.changeWorkingDirectory(remoteDirPath);
-                } else {
-                    System.out.println("Fallo al crear el directorio remoto.");
-                    return; // Importante salir si no se puede cambiar al directorio deseado.
-                }
-            }
+            // No es necesario verificar ni crear un directorio remoto nuevo.
+            // Directamente se procede a subir el archivo al directorio del usuario.
 
             // Asegúrate de especificar la ruta completa del archivo a subir.
-            String localFilePath = "/home/mballesterosv/prueba.txt";
+            String localFilePath = "/home/clientessh/prueba.txt"; // Asegúrate de que esta ruta sea correcta
             File uploadFile = new File(localFilePath);
-            try (FileInputStream fis = new FileInputStream(uploadFile)) {
-                client.setFileType(FTP.BINARY_FILE_TYPE);
-                boolean result = client.storeFile("prueba.txt", fis);
-                System.out.println("Subida realizada con éxito: " + result + ", " + client.getReplyString());
+            if (uploadFile.exists()) { // Verifica que el archivo exista antes de intentar subirlo
+                try (FileInputStream fis = new FileInputStream(uploadFile)) {
+                    client.setFileType(FTP.BINARY_FILE_TYPE);
+                    String remoteFileName = "prueba.txt"; // Nombre del archivo en el servidor
+                    boolean result = client.storeFile(remoteFileName, fis);
+                    System.out.println("Subida realizada con éxito: " + result + ", " + client.getReplyString());
+                }
+            } else {
+                System.out.println("El archivo local no existe.");
             }
 
         } catch (Exception e) {
