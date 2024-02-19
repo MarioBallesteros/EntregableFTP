929bb962a02ed96c3178167d896d8ad978d60896
MarioBallesteros <16maarioo16@gmail.com>
2024-02-19 13:46:09 +0100

    cambios cleinte

--- src/main/java/org/example/LaunchClient.java
+++ src/main/java/org/example/LaunchClient.java
@@ -5,23 +5,32 @@ import org.apache.commons.net.ftp.FTPClient;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.SocketException;
 
 public class LaunchClient {
     public static void main(String[] args) {
         FTPClient client = new FTPClient();
         try {
             // Especifica el puerto 2121 en la llamada connect
+// Conéctate al servidor y autentícate primero
             client.connect("192.168.1.45", 2121);
             client.login("anonymous", "password");
 
-            client.enterLocalPassiveMode();
-            client.setFileType(FTP.BINARY_FILE_TYPE);
+// Intenta cambiar al directorio remoto. Si falla, intenta crearlo.
+            String remoteDirPath = "/home/clientessh";
+            if (!client.changeWorkingDirectory(remoteDirPath)) {
+                if (client.makeDirectory(remoteDirPath)) {
+                    System.out.println("Directorio remoto creado con éxito.");
+                    client.changeWorkingDirectory(remoteDirPath);
+                } else {
+                    System.out.println("Fallo al crear el directorio remoto.");
+                    // Manejar el fallo adecuadamente
+                }
+            }
 
-            // Asegúrate de que el archivo que intentas subir existe y la ruta es correcta
-            String localFilePath = "/path/to/local/file.txt"; // Ruta local del archivo a subir
-            String remoteFile = "/home/anonimo"; // Ruta remota donde se subirá el archivo
-
-            File uploadFile = new File(localFilePath);
+            String remoteFile = "prueba.txt";
+            File uploadFile = new File("/path/to/local/file.txt");
             try (FileInputStream fis = new FileInputStream(uploadFile)) {
                 boolean result = client.storeFile(remoteFile, fis);
                 System.out.println("Subida realizada con éxito: " + result);
@@ -29,10 +38,10 @@ public class LaunchClient {
                 e.printStackTrace();
             }
 
-            client.logout();
-            client.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
