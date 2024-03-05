6d5f0eba6689ca1d6dcf898a2dd735a5aa06d1c9
psp <clase@gmail.com>
2024-03-05 19:48:33 +0100

    asd

--- src/main/java/org/example/LaunchServer.java
+++ src/main/java/org/example/LaunchServer.java
@@ -1,67 +1,43 @@
-import java.io.IOException;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
-import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTP;
+package org.example;
 
-public class ListaFicherosFTP {
+import org.apache.ftpserver.DataConnectionConfigurationFactory;
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
 
+public class LaunchServer {
     public static void main(String[] args) {
+        FtpServerFactory serverFactory = new FtpServerFactory();
+        ListenerFactory listenerFactory = new ListenerFactory();
 
-        if (args.length < 1) {
-            System.out.println("ERROR: indicar como parámetros:");
-            System.out.println("servidor [usuario] [contraseña]");
-            System.exit(1);
-        }
+        // Configuración del rango de puertos para el modo pasivo
+        DataConnectionConfigurationFactory dataConnectionConf = new DataConnectionConfigurationFactory();
 
-        String servidorFTP = args[0];
-        String usuario = args.length >= 2 ? args[1] : "anonymous";
-        String password = args.length >= 3 ? args[2] : "";
+        listenerFactory.setPort(2121);
+        listenerFactory.setDataConnectionConfiguration(dataConnectionConf.createDataConnectionConfiguration());
 
-        FTPClient clienteFTP = new FTPClient();
+        // Configurar usuario anónimo con acceso a una carpeta específica
+        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+        BaseUser user = new BaseUser();
+        user.setName("anonymous");
+        user.setPassword("anonymous");
+        user.setHomeDirectory("/home/anonimo");
 
         try {
-            clienteFTP.connect(servidorFTP);
-            int codResp = clienteFTP.getReplyCode();
-            if (!FTPReply.isPositiveCompletion(codResp)) {
-                System.out.printf("ERROR: Conexión rechazada con código de respuesta %d.\n", codResp);
-                System.exit(2);
-            }
-
-            clienteFTP.enterLocalPassiveMode();
-            clienteFTP.setFileType(FTP.BINARY_FILE_TYPE);
-
-            boolean loginOK = clienteFTP.login(usuario, password);
-            if (!loginOK) {
-                System.out.printf("ERROR: Login con usuario %s rechazado.\n", usuario);
-                return;
-            }
-
-            System.out.printf("INFO: Conexión establecida. Mensaje de bienvenida del servidor:\n====\n%s\n====\n", clienteFTP.getReplyString());
-            System.out.printf("INFO: Directorio actual en servidor: %s. Contenidos:\n", clienteFTP.printWorkingDirectory());
-
-            FTPFile[] fichServ = clienteFTP.listFiles();
-            for (FTPFile f : fichServ) {
-                String infoAdicFich = "";
-                if (f.getType() == FTPFile.DIRECTORY_TYPE) {
-                    infoAdicFich = "/";
-                } else if (f.getType() == FTPFile.SYMBOLIC_LINK_TYPE) {
-                    infoAdicFich = " -> " + f.getLink();
-                }
-                System.out.printf("%s%s\n", f.getName(), infoAdicFich);
-            }
-        } catch (IOException e) {
-            System.out.println("ERROR: conectando al servidor");
+            serverFactory.getUserManager().save(user);
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (clienteFTP.isConnected()) {
-                try {
-                    clienteFTP.disconnect();
-                    System.out.println("INFO: conexión cerrada.");
-                } catch (IOException e) {
-                    System.out.println("AVISO: no se pudo cerrar la conexión.");
-                }
-            }
+        }
+
+        serverFactory.addListener("default", listenerFactory.createListener());
+        FtpServer server = serverFactory.createServer();
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
